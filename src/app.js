28b7c72264040a00be8e46c4a314d82a5cd7d3d9
Nguyen The Hien <118051299+hiennt200210@users.noreply.github.com>
2023-12-02 22:21:17 +0700

    fix: disable cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 // Connect to the database and initialize tables
 require("./models/index");
 
+const cors = require("cors");
 const express = require("express");
 const path = require("path");
 const errorHandler = require("./middlewares/handleErrors");
@@ -13,6 +14,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Using middleware
+app.use(cors);
 app.use(express.static(path.join(__dirname, "statics")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
