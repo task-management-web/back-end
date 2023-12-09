2aca398ab2b59a5a3ecb4b74c3f77f673cb0e79f
Hiền Hiềnn <hien.tt161102@gmail.com>
2023-12-09 16:41:45 +0700

    get all boards app

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -1,11 +1,13 @@
-const { Op } = require("sequelize");
-const enums = require("../helpers/enums");
-const resources = require("../helpers/resources");
-const Board = require("../models/board");
-const BoardMember = require("../models/boardMember");
-const User = require("../models/user");
-const BadRequest = require("../errors/BadRequest");
-const NotFound = require("../errors/NotFound");
+/** @format */
+
+const { Op } = require('sequelize');
+const enums = require('../helpers/enums');
+const resources = require('../helpers/resources');
+const Board = require('../models/board');
+const BoardMember = require('../models/boardMember');
+const User = require('../models/user');
+const BadRequest = require('../errors/BadRequest');
+const NotFound = require('../errors/NotFound');
 
 //const { checkTitle, checkDescription } = require("../helpers/boardValidation");
 
@@ -13,92 +15,99 @@ const NotFound = require("../errors/NotFound");
  * Lấy thông tin tất cả các bảng của người dùng.
  */
 async function getAllBoards(req, res, next) {
-    // Do something...
+	try {
+		const allBoards = await Board.findAll();
+
+		res.json(allBoards);
+	} catch (error) {
+		console.error('Error fetching all boards:', error);
+		res.status(500).json({ error: 'Could not fetch all boards' });
+	}
 }
 
 /*
  * Lấy thông tin một bảng theo ID.
  */
 async function getBoardById(req, res, next) {
-    try {
-        const board = await Board.findOne({
-            where: {
-                id: req.params.id,
-            },
-            include: User,
-        });
+	try {
+		const board = await Board.findOne({
+			where: {
+				id: req.params.id,
+			},
+			include: User,
+		});
 
-        if (!board) {
-            return next(new NotFound());
-        }
+		if (!board) {
+			return next(new NotFound());
+		}
 
-        res.status(200).json(board);
-    } catch (error) {
-        next(error);
-    }
+		res.status(200).json(board);
+	} catch (error) {
+		next(error);
+	}
 }
 
 /*
  * Kiểm tra định dạng dữ liệu bảng.
  */
 function checkBoard(board) {
-    const { title, description, backgroundUrl } = board;
-    let errors = {};
+	const { title, description, backgroundUrl } = board;
+	let errors = {};
 
-    checkTitle(title, errors);
-    checkDescription(description, errors);
+	checkTitle(title, errors);
+	checkDescription(description, errors);
 
-    if (Object.keys(errors).length !== 0) {
-        throw new BadRequest(errors);
-    }
+	if (Object.keys(errors).length !== 0) {
+		throw new BadRequest(errors);
+	}
 }
 
 /*
  * Tạo bảng.
  */
 async function createBoard(req, res, next) {
-    const board = req.body;
+	const board = req.body;
 
-    try {
-        // Kiểm tra định dạng dữ liệu
-        checkBoard(board);
+	try {
+		// Kiểm tra định dạng dữ liệu
+		checkBoard(board);
 
-        // Lưu dữ liệu vào database
-        const boardInserted = await Board.create(board);
+		// Lưu dữ liệu vào database
+		const boardInserted = await Board.create(board);
 
-        await BoardMember.create({
-            UserId: req.user.id,
-            BoardId: boardInserted.id,
-            role: enums.role.admin,
-        });
+		await BoardMember.create({
+			UserId: req.user.id,
+			BoardId: boardInserted.id,
+			role: enums.role.admin,
+		});
 
-        res.status(201).json({
-            message: resources.createBoardSuccessfully,
-            boardId: boardInserted.id,
-        });
-    } catch (error) {
-        next(error);
-    }
+		res.status(201).json({
+			message: resources.createBoardSuccessfully,
+			boardId: boardInserted.id,
+		});
+	} catch (error) {
+		next(error);
+	}
 }
 
 /*
  * Cập nhật thông tin bảng.
  */
 async function updateBoard(req, res, next) {
-    // Do something ...
+	// Do something ...
 }
 
 /*
  * Đóng bảng.
  */
 async function closeBoard(req, res, next) {
-    // Do something ...
+	// Do something ...
 }
 
 module.exports = {
-    createBoard,
-    getAllBoards,
-    getBoardById,
-    updateBoard,
-    closeBoard,
+	createBoard,
+	getAllBoards,
+	getBoardById,
+	updateBoard,
+	closeBoard,
 };
